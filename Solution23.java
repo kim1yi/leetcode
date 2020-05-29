8f5e07002b54a62ec86d17dd2d0c3705bb0676ae
k1myi <k1myi@outlook.com>
2020-05-29 22:54:01 +0800

    different two way to use PriorityQueue

--- Solution23.java
+++ Solution23.java
@@ -7,38 +7,51 @@ class ListNode {
         int val;
         ListNode next;
         ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        ListNode(final int val) { this.val = val; }
+        ListNode(final int val, final ListNode next) { this.val = val; this.next = next; }
 }
 
 public class Solution23 {
-    public ListNode mergeKLists(ListNode[] lists) {
+    public ListNode mergeKLists(final ListNode[] lists) {
         
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode o1, ListNode o2) {
-                if (o1.val < o2.val)
-                    return -1;
-                else if (o1.val == o2.val)
-                    return 0;
-                else
-                    return 1;
-            }
-        });
+        int size = lists.length;
+        if (size == 0 || lists[0] == null)
+            return null;
 
-        for(ListNode node : lists) {
-            pq.add(node);
+        //Comparator的写法，利用lambdma
+        Comparator<ListNode> comparator = (o1, o2)->{return o1.val - o2.val;};
+        final PriorityQueue<ListNode> pq = new PriorityQueue<>(size, comparator);
+
+        //另一种写法，覆盖compare方法
+        // PriorityQueue<ListNode> pq = new PriorityQueue<>(size, new Comparator<ListNode>() {
+        //     @Override
+        //     public int compare(ListNode o1, ListNode o2) {
+        //         if (o1.val < o2.val)
+        //             return -1;
+        //         else if (o1.val == o2.val)
+        //             return 0;
+        //         else
+        //             return 1;
+        //     }
+        // });
+
+        for(final ListNode node : lists) {
+            //这个判断很重要，null对象也会改变lists的length大小
+            if (node != null)
+                pq.add(node);
         }
 
-        ListNode head = pq.poll();
-        ListNode next = head.next;
-        if (next!=null)
-            pq.add(next);
+        //总是会遇到这样一个问题，直接在原链表上进行修改的时候总是不知道该怎么处理入口
+        //下次一定要记住用一个新的空头来作为入口！！！
+        final ListNode head = new ListNode();
+        ListNode tail = head;
+
         while (!pq.isEmpty()) {
-            next = pq.poll();
-            if (next.next != null) {
-                pq.add(next.next);
-                next = next.next;
+            tail.next = pq.poll();
+            tail = tail.next;
+
+            if (tail.next != null) {
+                pq.add(tail.next);
             }
         }
 
