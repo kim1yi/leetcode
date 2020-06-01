746cd8d1cfa2186c48ceed0282b956cc79ee24de
k1myi <k1myi@outlook.com>
2020-06-01 16:50:15 +0800

    Leetcode 55 Jump Game

---
+++ Solution55.java
@@ -0,0 +1,24 @@
+public class Solution55 {
+    public boolean canJump(int[] nums) {
+        int size = nums.length;
+        int last=size-1,i;
+        for(i=size-2;i>=0;i--){
+            //意味着可以到达这一格
+            if(i+nums[i]>=last) {
+                last=i;
+            }
+        }
+        //判断最后能到达的那一格是否是刚开始那格
+        return last<=0;
+    }
+
+    public boolean myCanJump(int[] nums) {
+        int size = nums.length;
+        int maxLocation = 0;
+        for (int i = 0; i < size; i++) {
+            if (maxLocation < i) return false;
+            maxLocation = Math.max(maxLocation, i+nums[i]);
+        }
+        return true;
+    }
+}
